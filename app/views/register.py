ab95d3b208cd47133cb05d7c32075e3be1284983
fan <532898388@qq.com>
2018-03-28 09:39:38 +0800

    限制用户名/密码长度

--- app/views/register.py
+++ app/views/register.py
@@ -17,20 +17,25 @@ def index():
         g.request_passwd = passwd
         g.request_confirm_password = confirm_password
         g.full_name = full_name
-        user = db.session.query(Users).filter_by(username=username).first()  # 查询用户信息
-        if user is None:
-            if passwd != confirm_password:
-                flash(u'两次密码输入不一致!')
-            else:
-                UserInfo = mysqld.Users(
-                    username=username,
-                    passwd=generate_password_hash(passwd),
-                    full_name=full_name
-                )
-                db.session.add(UserInfo)
-                db.session.commit()
-                flash(u'注册成功,请登录!')
+        if len(username) < 5:
+            flash(u'用户名不能少于5个字符!')
+        elif len(passwd) < 8:
+            flash(u'密码不能少于8个字符!')
         else:
-            flash(u'用户名已存在!')
+            user = db.session.query(Users).filter_by(username=username).first()  # 查询用户信息
+            if user is None:
+                if passwd != confirm_password:
+                    flash(u'两次密码输入不一致!')
+                else:
+                    UserInfo = mysqld.Users(
+                        username=username,
+                        passwd=generate_password_hash(passwd),
+                        full_name=full_name
+                    )
+                    db.session.add(UserInfo)
+                    db.session.commit()
+                    flash(u'注册成功,请登录!')
+            else:
+                flash(u'用户名已存在!')
 
     return render_template("register/index.html")
